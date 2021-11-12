60330e748a9886b507e4d08bccde0509d21dc16c
zhangv16 <vz89@live.jp>
2021-11-12 02:46:03 -0500

    changes

--- app/src/main/java/com/mer/plamer/usecases/PlaylistAction.java
+++ app/src/main/java/com/mer/plamer/usecases/PlaylistAction.java
@@ -2,18 +2,99 @@ package com.mer.plamer.usecases;
 
 import com.mer.plamer.entities.Playlist;
 import com.mer.plamer.entities.Track;
+import java.util.Comparator;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class PlaylistAction{
+/**
+ * Actions that a playlist can perform
+ */
+public class PlaylistAction {
 
-    public boolean delTrack(Playlist playlist, Track track) {
-        return playlist.delTrack(track);
+    private final Playlist playlist;
+
+    /**
+     * Constructor for PlaylistAction.
+     * @param playlist The playlist we want to perform action on.
+     */
+    public PlaylistAction(Playlist playlist) {
+        this.playlist = playlist;
     }
 
-    public void search() {
-
+    /**
+     * Add new track to the playlist if the track is not already in the playlist.
+     * @param track the track that we wanted to add to the playlist
+     * @return whether the track has been successfully added
+     */
+    public boolean addTrack(Track track) {
+        return this.playlist.addTrack(track);
     }
 
-    public void add() {
+    /**
+     * Delete track from the playlist if the track is in the playlist.
+     * @param track the track that we wanted to delete from the playlist
+     * @return whether the track has been successfully deleted or not
+     */
+    public boolean delTrack(Track track) {
+        return this.playlist.delTrack(track);
+    }
 
+    /**
+     * Sort the playlist by title.
+     */
+    public void sortByTitle() {
+        this.playlist.sort(new Comparator<Track>() {
+            @Override
+            public int compare(Track o1, Track o2) {
+                return o1.getTitle().compareTo(o2.getTitle());
+            }
+        });
+    }
+
+    /**
+     * Sort the playlist by artist.
+     */
+    public void sortByArtist() {
+        this.playlist.sort(new Comparator<Track>() {
+            @Override
+            public int compare(Track o1, Track o2) {
+                return o1.getArtist().compareTo(o2.getArtist());
+            }
+        });
+    }
+
+    /**
+     * Sort the playlist by length.
+     */
+    public void sortByLength() {
+        this.playlist.sort(new Comparator<Track>() {
+            @Override
+            public int compare(Track o1, Track o2) {
+                return o1.getLength().compareTo(o2.getLength());
+            }
+        });
+    }
+
+    /**
+     * Sort the playlist randomly.
+     */
+    public void sortByRandom() {
+        this.playlist.sort(new Comparator<Track>() {
+            @Override
+            public int compare(Track o1, Track o2) {
+                Random rnd = ThreadLocalRandom.current();
+                if (rnd.nextInt(2) == 0) return -1;
+                return 1;
+            }
+        });
+    }
+
+    /**
+     * Set the status of Playlist if it is a valid status.
+     * @param status The status that the playlist wanted to change to.
+     * @return whether the playlist has been successfully to this status.
+     */
+    public boolean setStatus(String status) {
+        return this.playlist.setStatus(status);
     }
 }
