7b255f9427d2430203a88a1996379b374316074a
zhangv16 <vz89@live.jp>
2021-11-12 17:28:09 -0500

    changes

--- app/src/main/java/com/mer/plamer/entities/PlaylistLibrary.java
+++ app/src/main/java/com/mer/plamer/entities/PlaylistLibrary.java
@@ -2,28 +2,55 @@ package com.mer.plamer.entities;
 
 import java.util.ArrayList;
 
+/**
+ * Library of playlists.
+ */
 public class PlaylistLibrary implements Storable<Playlist> {
-    private ArrayList<Playlist> playlistList;
 
-    public PlaylistLibrary(){
-    }
+    private ArrayList<Playlist> playlists;
 
+    /**
+     * Constructor for PlaylistLibrary.
+     */
+    public PlaylistLibrary() {}
+
+    /**
+     * Add playlist to this playlist library.
+     * @param playlist the playlist we wanted to add to.
+     */
     @Override
-    public void add(Playlist new_playlist) {
+    public void add(Playlist playlist) {
+        this.playlists.add(playlist);
     }
 
+    /**
+     *
+     * @param name
+     * @return
+     */
     @Override
     public boolean remove(String name) {
-        return true;
+        for (Playlist p : this.playlists) {
+            if (p.getName().equals(name)) {
+                this.playlists.remove(p);
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean isEmpty() {
-        return true;
+        return this.playlists.size() == 0;
     }
 
     @Override
-    public Playlist contain(String obj) {
+    public Playlist contain(String name) {
+        for (Playlist p : this.playlists) {
+            if (p.getName().equals(name)) {
+                return p;
+            }
+        }
         return null;
     }
 
