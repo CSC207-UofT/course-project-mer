31fc90fa795175d68023c4f4b3ad9d5bc792c299
RuYi-NG <josephshijunyan@gmail.com>
2021-11-26 00:23:39 -0500

    stable search methods before working on search control

--- app/src/main/java/com/mer/plamer/controller/SearchControl.java
+++ app/src/main/java/com/mer/plamer/controller/SearchControl.java
@@ -1,17 +1,27 @@
 package com.mer.plamer.controller;
 
 import com.mer.plamer.usecases.LibraryAction;
+import com.mer.plamer.usecases.UserLibraryAction;
+import com.mer.plamer.usecases.PlaylistLibraryAction;
+import com.mer.plamer.usecases.TrackLibraryAction;
 
 public class SearchControl {
     LibraryAction libraryAction;
 
+    public static TrackLibraryAction trackLibraryAction = new TrackLibraryAction();
+    public static UserLibraryAction userLibraryAction = new UserLibraryAction();
+    public static PlaylistLibraryAction playlistLibraryAction = new PlaylistLibraryAction();
+
     public SearchControl() {
+
     }
 
     public void searchTrack() {
+
     }
 
-    public void searchPlaylist() {
+    public void searchPlaylist(String keyword) {
+
     }
 
     public void searchUser() {
